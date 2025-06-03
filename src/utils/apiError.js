099e3c80bf4d0f25ba03e9da6b41b9568ed0c89d
Ashikur Rahman Hridoy <ashikur6053@gmail.com>
2025-06-04 01:49:27 +0600

    Refactor ApiError and ApiSuccess classes for improved error handling and response structure

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,12 +1,73 @@
 class ApiError extends Error {
-  constructor(statusCode, message, errors) {
+  //  @param {number} statuscode -the HTTP status code for the error
+  //  @param {string} message  -the error message
+  //  @param {object} erros -additional error details (optional)
+
+  constructor(
+    statuscode,
+    message = "Somthing went wrong ",
+    errors = {},
+    stack = " ",
+    errorCode = ""
+  ) {
     super(message);
-    this.success = false;
-    this.statusCode = statusCode;
-    this.message = message ?? "Something went wrong";
-    this.errors = errors ?? [];
-    this.stack = Error.captureStackTrace(this, this.constructor);
+
+    (this.name = this.constructor.name), (this.success = false);
+    this.statuscode = statuscode;
+    this.message = message;
+    this.errors = errors;
+    this.errorCode = errorCode;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+
+  //401 unauthorized
+  static unauthorized(message = "Unauthorized", errors = {}, errorCode = "AUTH_401") {
+    return new ApiError(401, message, errors, " ", errorCode);
+  }
+
+  //403 forbidden
+  static forbidden(message = "Forbidden", errors = {}, errorCode = "AUTH_403") {
+    return new ApiError(403, message, errors, " ", errorCode);
+  }
+
+  //400 Bad Request
+  static badRequest(message = "Bad Request", errors = {}, errorCode = "REQ_400") {
+    return new ApiError(400, message, errors, " ", errorCode);
+  }
+
+  //404 Not Found
+  static notFound(message = "Not Found", errors = {}, errorCode = "REQ_404") {
+    return new ApiError(404, message, errors, " ", errorCode);
+  }
+
+  //409 Conflict
+  static conflict(message = "Conflict", errors = {}, errorCode = "REQ_409") {
+    return new ApiError(409, message, errors, " ", errorCode);
+  }
+
+  //500 Server Error
+  static serverError(message = "Internal Server Error ", errors = {}, errorCode = "SERVER_500") {
+    return new ApiError(500, message, errors, " ", errorCode);
+  }
+
+  //204 No Content
+  static noContent(message = "No Content") {
+    return new ApiError(204, message);
+  }
+
+  //Database Error
+  static databaseError(message = "Database Error", errors = {}, errorCode = "DB_500 ") {
+    return new ApiError(500, message, errors, " ", errorCode);
+  }
+
+  //Custom Error
+  static custom(statuscode, message, errors = {}, stack = " ", errorCode = " ") {
+    return new ApiError(statuscode, message, errors, stack, errorCode);
   }
 }
-
 export default ApiError;
--- src/utils/apiSuccess.js
+++ src/utils/apiSuccess.js
@@ -1,10 +1,30 @@
 class ApiSuccess {
-  constructor(statuscode, message, data = {}) {
-    this.success = statuscode < 400;
-    this.statuscode = statuscode;
+  constructor(statusCode = 200, message = "Success", data = {}, meta = {}) {
+    this.success = statusCode < 400;
+    this.statusCode = statusCode;
     this.message = message;
     this.data = data;
+    this.meta = meta;
+  }
+
+  //200 ok
+  static ok(message = "Ok", data = {}, meta = {}) {
+    return new ApiSuccess(200, message, data, meta);
+  }
+
+  //201 created
+  static created(message = "Resource created", data = {}, meta = {}) {
+    return new ApiSuccess(201, message, data, meta);
+  }
+
+  //204 NO Content(rare,Data will be empty)
+  static noContent(message = "No Content") {
+    return new ApiSuccess(204, message);
+  }
+
+  //Custom success
+  static custom(statusCode, message, data = {}, meta = {}) {
+    return new ApiSuccess(statusCode, message, data, meta);
   }
 }
-
 export default ApiSuccess;
