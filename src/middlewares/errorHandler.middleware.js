ec152de3fa8e850dfa3b717e78cf29949f7e419f
Ashikur Rahman Hridoy <ashikur6053@gmail.com>
2025-06-18 11:34:48 +0600

    Refactor error handler middleware to improve response structure and error handling

--- src/middlewares/errorHandler.middleware.js
+++ src/middlewares/errorHandler.middleware.js
@@ -1,6 +1,11 @@
 import ApiError from "../utils/apiError.js";
 
-const errorHandler = (err) => {
-  throw ApiError.serverError(err.message, err.errors, err.errorCode);
+const errorHandler = (err, _req, res, _next) => {
+  const { statusCode, message, errors, errorCode, stack } = err;
+
+  return res
+    .status(statusCode || 500)
+    .json(ApiError.custom(statusCode, message, errors, stack, errorCode));
 };
+
 export default errorHandler;
