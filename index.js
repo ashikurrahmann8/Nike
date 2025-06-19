4988510108c89eab2d938a80fdf1806edc9f5259
Ashikur Rahman Hridoy <ashikur6053@gmail.com>
2025-06-19 19:41:30 +0600

    Refactor server startup logic and enhance database connection handling

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import { app } from "./src/app.js";
 import { PORT } from "./src/constants/constants.js";
+import dbConnection from "./src/db/index.js";
 
-app.listen(PORT, () => {
-  console.log(`server is running http://localhost:${PORT}`);
-});
+const serverStart = async () => {
+  try {
+    await dbConnection();
+    app.listen(PORT, () => {
+      console.log(`server is running http://localhost:${PORT}/`);
+    });
+  } catch (error) {
+    console.log(" ~ file: index.js:50 ~ serverStart ~ error:", error);
+  }
+};
+
+serverStart();
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,5 @@
 import "dotenv/config";
 const PORT = process.env.PORT || 8000;
 const WHITELIST = process.env.WHITELIST || ["http://localhost:5175"];
-export { PORT, WHITELIST };
+const MONGO_URI = process.env.MONGO_URI;
+export { PORT, WHITELIST, MONGO_URI };
