791c9fd20c756379ccaddac807e67c8283ea68ba
Ashikur Rahman Hridoy <ashikur6053@gmail.com>
2025-06-20 16:37:53 +0600

    Add user model with schema definition for user data management

---
+++ src/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+    },
+    firstName: {
+      type: String,
+      required: [true, "Firstname is required"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Lastname is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive", "suspended"],
+      default: "active",
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.models.User || mongoose.model("User", userSchema);
