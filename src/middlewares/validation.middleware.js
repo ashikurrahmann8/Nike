37a5e208281d376f6181c58a972e95969ea329c6
Ashikur Rahman Hridoy <ashikur6053@gmail.com>
2025-07-20 18:26:51 +0600

    Refactor validation middleware to remove unused response parameter and clean up error handling

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -2,7 +2,7 @@ import { success, z, ZodError } from "zod";
 import ApiError from "../utils/apiError.js";
 
 function validationMiddleware(schema) {
-  return async (req, res, next) => {
+  return async (req, _res, next) => {
     try {
       const validateData = schema.parse(req.body);
       req.body = validateData;
@@ -11,7 +11,7 @@ function validationMiddleware(schema) {
       if (error instanceof ZodError) {
         const { fieldErrors } = z.flattenError(error);
         throw ApiError.badRequest("ValidationError", fieldErrors);
-        // return res.status(400).json({ success: false, errors: error.ZodError });
+        
       } else {
         throw ApiError.serverError(error.message);
       }
