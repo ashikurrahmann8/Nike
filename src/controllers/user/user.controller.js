7d1bfa65264769f6d6d230ec944f9d9a1009e357
Ashikur Rahman Hridoy <ashikur6053@gmail.com>
2025-07-22 10:45:44 +0600

    Add user password hashing and validation checks in signup process

--- src/controllers/user/user.controller.js
+++ src/controllers/user/user.controller.js
@@ -1,8 +1,23 @@
 import { User } from "../../models/user.model.js";
+import ApiError from "../../utils/apiError.js";
+import ApiSuccess from "../../utils/apiSuccess.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
 
 const signup = asyncHandler(async (req, res) => {
   const { userName, name, email, password } = req.body;
+  const userNameExist = await User.findOne({ userName });
+  if (userNameExist) {
+    throw ApiError.badRequest("Username already exists");
+  }
+  const emailExist = await User.findOne({ email });
+  if (emailExist) {
+    throw ApiError.badRequest("Email already exists");
+  }
+  await User.create({ userName, name, email, password });
+  const user = await User.findOne({ email }).select(
+    "-__v -updatedAt -createdAt -password -passwordResetToken -passwordResetExpires"
+  );
+  return res.status(200).json(ApiSuccess.created("User created", user));
 });
 
 export { signup };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new Schema(
   {
@@ -40,5 +41,9 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
-
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password") || !this.password) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 export const User = mongoose.models.User || mongoose.model("User", userSchema);
