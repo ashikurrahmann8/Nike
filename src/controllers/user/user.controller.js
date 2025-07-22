ef1a830d6d6f1156bccf73195f6f3db02aedc165
Ashikur Rahman Hridoy <ashikur6053@gmail.com>
2025-07-22 11:30:19 +0600

    Refactor signup controller to use created user object for fetching user details

--- src/controllers/user/user.controller.js
+++ src/controllers/user/user.controller.js
@@ -13,10 +13,13 @@ const signup = asyncHandler(async (req, res) => {
   if (emailExist) {
     throw ApiError.badRequest("Email already exists");
   }
-  await User.create({ userName, name, email, password });
-  const user = await User.findOne({ email }).select(
+
+  const createdUser = await User.create({ userName, name, email, password });
+
+  const user = await User.findById(createdUser._id).select(
     "-__v -updatedAt -createdAt -password -passwordResetToken -passwordResetExpires"
   );
+
   return res.status(200).json(ApiSuccess.created("User created", user));
 });
 
