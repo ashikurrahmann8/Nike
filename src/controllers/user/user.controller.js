6e84573760d97933d0200804155d58f1c8fdfaa7
Ashikur Rahman Hridoy <ashikur6053@gmail.com>
2025-07-30 20:32:44 +0600

    Add email verification functionality with JWT support in signup process

--- src/controllers/user/user.controller.js
+++ src/controllers/user/user.controller.js
@@ -1,9 +1,10 @@
-import { APP_URL } from "../../constants/constants.js";
+import { APP_URL, JWT_SECRET } from "../../constants/constants.js";
 import { User } from "../../models/user.model.js";
 import ApiError from "../../utils/apiError.js";
 import ApiSuccess from "../../utils/apiSuccess.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
 import { sendMail, verifyEmail } from "../../utils/mail.js";
+import jwt from "jsonwebtoken";
 
 const signup = asyncHandler(async (req, res) => {
   const { userName, name, email, password } = req.body;
@@ -23,7 +24,7 @@ const signup = asyncHandler(async (req, res) => {
   );
 
   const token = user.jwtToken();
-  const verifyUrl = `${APP_URL}/api/v1/users/verify/?token=${token}`;
+  const verifyUrl = `${APP_URL}/api/v1/users/verify?token=${token}`;
   sendMail({
     email,
     subject: "Verify your email",
@@ -33,4 +34,31 @@ const signup = asyncHandler(async (req, res) => {
   return res.status(200).json(ApiSuccess.created("User created", user));
 });
 
-export { signup };
+const verifymail = asyncHandler(async (req, res) => {
+  const { token } = req.query;
+  if (!token) {
+    throw ApiError.badRequest("Token is required");
+  }
+
+  const decodedToken = jwt.verify(token, JWT_SECRET);
+  if (!decodedToken) {
+    throw ApiError.badRequest("Invalid token");
+  }
+
+  const user = await User.findById(decodedToken.id).select(
+    "-__v -updatedAt -createdAt -password -passwordResetToken -passwordResetExpires"
+  );
+
+  if (!user) {
+    throw ApiError.notFound("User not found");
+  }
+  if (user.isVerified) {
+    return res.status(200).json(ApiSuccess.ok("User is already verified", user));
+  } else {
+    user.isVerified = true;
+    await user.save();
+    return res.status(200).json(ApiSuccess.ok("User verified", user));
+  }
+});
+
+export { signup, verifymail };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,10 @@
 import e from "express";
-import { signup } from "../controllers/user/user.controller.js";
+import { signup, verifymail } from "../controllers/user/user.controller.js";
 import validationMiddleware from "../middlewares/validation.middleware.js";
 import { userSignupSchema } from "../vallidators/user.vallidator.js";
 
 const router = e.Router();
 router.post("/users/signup", validationMiddleware(userSignupSchema), signup);
+router.get("/users/verify/", verifymail);
+
 export default router;
