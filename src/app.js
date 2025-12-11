029307b6c229e13766cceab5f804bc3bd4cde8ee
Ashikur Rahman Hridoy <ashikur6053@gmail.com>
2025-12-11 08:05:47 +0600

    feat: add express-rate-limit middleware to limit request rates

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import { ACCESS_TOKEN_SECRET, WHITELIST } from "./constants/constants.js";
 import cookieParser from "cookie-parser";
 import errorHandler from "./middlewares/errorHandler.middleware.js";
-
+import rateLimit from "express-rate-limit";
 const app = express();
 
 app.use(express.json());
@@ -24,6 +24,19 @@ app.use(
 
 app.use(cookieParser(ACCESS_TOKEN_SECRET));
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: (req, _res) => (req.user ? 100 : 10),
+  standardHeaders: "draft-8",
+  legacyHeaders: true,
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  // store: ... , // Redis, Memcached, etc. See below.
+  message: "True many requests from this IP, please try again after 15 minutes.",
+  keyGenerator: (req) => req.ip,
+});
+
+app.use("/api/v1", limiter);
+
 //define routes
 
 import heathCheckRoute from "./routes/heathCheck.route.js";
@@ -36,4 +49,5 @@ app.use("/api/v1/users", userRoute);
 app.use("/api/v1", categoryRoute);
 app.use("/api/v1", subcategoryRoute);
 app.use(errorHandler);
+
 export { app };
